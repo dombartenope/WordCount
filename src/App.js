43255e700cbb85e852a50c649382c3b929b239f5
Dominick Bartenope <dominickbartenope@yahoo.com>
2020-01-26 11:44:19 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+
+
+  return(
+    <div>
+      <h1>How fast can you type?</h1>
+      <textarea 
+        // onChange={handleChange}
+        // value={text}
+      />
+      <h4>Time Remaining:</h4>
+      <button>Start</button>
+      <h1>Word Count:</h1>
     </div>
-  );
+  )
 }
-
-export default App;
